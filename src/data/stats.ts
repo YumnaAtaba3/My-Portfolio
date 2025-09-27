9943cd4cae356175833ceaf217e8a9c8e1fc7c3a
YumnaAtaba3 <yumnaat33@gmail.com>
2025-09-27 23:05:34 +0300

    "edit projects data"
    Signed-off-by: YumnaAtaba3 <yumnaat33@gmail.com>

--- src/data/stats.ts
+++ src/data/stats.ts
@@ -5,7 +5,7 @@
 
 
 export const stats: Stat[] = [
-  { label: "Total Projects", value: "3+" },
+  { label: "Total Projects", value: "6+" },
   { label: "Technologies", value: "8+" },
   { label: "Code Quality", value: "95%" },
   { label: "On-Time Delivery", value: "100%" },
