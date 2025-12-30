e7e1226db68e3c7ec733739e3d4f307b69dfa5fe
YumnaAtaba3 <yumnaat33@gmail.com>
2025-12-30 15:53:38 +0300

    return every thing
    Signed-off-by: YumnaAtaba3 <yumnaat33@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 const repoName = "My-Portfolio";
 
+ 
+
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: `/${repoName}/`, // important for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    sourcemap: false,
   },
 });
